52ea0c0c5af96a451ce8251e3f8cbce81bde0738
Aaron Leon <aaronleon2018@u.northwestern.edu>
2017-04-21 15:31:20 -0500

    TESTS

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use std::env;
 use std::fs::File;
-use std::io::{stdin, Read};
+use std::io::{stdin, Read, stdout, Write};
 use std::collections::{HashMap, HashSet};
-
-//mod trie;
 
 const ALPHABET: [char; 26] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 fn main() {
@@ -13,29 +11,29 @@ fn main() {
     let user_input = read_input(stdin());
 
     for s in &user_input {
-        spell_check(&s.to_lowercase(), &dictionary)
+        spell_check(stdout(), &s.to_lowercase(), &dictionary, 2)
     }
 }
 
-fn spell_check(input:&str, dictionary:&HashMap<String, usize>) {
 
-    if let Some(result) = dictionary.get(input) {
-        // if input is correctly spelt
-        return println!("{}", input);
+fn spell_check<W:Write>(mut writer:W, input:&str, dictionary:&HashMap<String, usize>, edit_distance:usize) {
+    if dictionary.contains_key(input) {
+        return write!(writer, "{}", input).unwrap();
     }
 
     let mut candidate_words:HashSet<String> = HashSet::new();
     candidate_words.insert(input.to_string());
 
-    check_edits(&mut candidate_words);
-    check_edits(&mut candidate_words);
+    for _ in 0..edit_distance {
+        check_edits(&mut candidate_words);
+    }
 
     let correct_word:String = find_probable_match(dictionary, candidate_words);
     // if input is incorrectly spelt
     if correct_word.is_empty() {
-        println!("{}, -", input);
+        write!(writer, "{}, -", input).unwrap();
     } else {
-        println!("{},{}", input, correct_word);
+        write!(writer, "{}, {}", input, correct_word).unwrap();
     }
 }
 
@@ -81,7 +79,7 @@ fn check_insert(input:&str, words:&mut HashSet<String>, index:usize){
 fn check_delete(input:&str, words:&mut HashSet<String>, index:usize) {
     let mut word:String = "".to_string();
     let input_string: String = input.to_string();
-    // let left: String = (input[..index]).to_owned();
+    
     for (pos, c) in input_string.chars().enumerate() {
         if pos == index{
             continue;
@@ -91,73 +89,30 @@ fn check_delete(input:&str, words:&mut HashSet<String>, index:usize) {
         }
     }
     words.insert(word.to_owned());
-    // let word:String = left + &input[index+1..];
-    // words.insert(word);
 }
 
 fn check_replace(input:&str, words:&mut HashSet<String>, index:usize) {
-    // let mut word = "";
-    let mut temp_word:String = "".to_owned();
-    let input_string: String = input.to_string();
+    let prefix = input[..index].to_string();
+    let suffix = input[index + 1..].to_string();
     for letter in ALPHABET.into_iter() {
-        if input.len() < 1 {
-            // word = letter.to_string();
-            continue;
-        }
-        else {
-            for (pos, c) in input_string.chars().enumerate() {
-                if pos == index{
-                    // let repl: char = *letter;
-                    // let temp_word:String = format!("{}{}", word, repl);
-                    // word = temp_word.as_str();
-                    temp_word.push(*letter);
-                }
-                else{ 
-                    // let temp_word:String = format!("{}{}", word,c);
-                    // word = temp_word.as_str();
-                    temp_word.push(c);
-                }
-            }
-        }
-
-         if temp_word.len() > 0{
-            words.insert(temp_word.clone());
-        }     
+        let temp = format!("{}{}{}", prefix, letter.to_string(), suffix);
+        words.insert(temp);
     }
 }
 
 fn check_transpose(input:&str, words:&mut HashSet<String>, index:usize) {
-	// let mut word = "";
-    let mut temp_word:String = "".to_owned();
-    let input_string:String = input.to_string();
-    if input_string.len() <= 1{
-        words.insert(input_string);    
+    if index >= input.len()-1 {
+        return 
     }
-    else{
-        for (pos, c) in input_string.chars().enumerate() {
-            if pos == index {
-                if index+1 < input_string.len(){
-                    let next: char = input_string.chars().nth(index+1).unwrap();
-                    // temp_word = format!("{}{}{}", word, next, c);
-                    // temp_word.push_str(word);
-                    temp_word.push(next);
-                    temp_word.push(c);
-                    
-                }
-            }
-            else{ 
-                // temp_word.push_str(word);
-                temp_word.push(c);    
-                // let temp_word:String = word.to_string();
-                // temp_word.push(c);
-                // let temp_word:String = format!("{}{}", word,c);
-                // word = temp_word.as_str();
-            }
-        }
-    }
-    if temp_word.len() > 0{
-            words.insert(temp_word.clone());
-    }  
+
+    let prefix = input[..index].to_string();
+    let suffix = input[index+2..].to_string();
+
+    let curr:char = input.chars().nth(index).unwrap();
+    let next:char = input.chars().nth(index+1).unwrap();
+    let temp = format!("{}{}{}{}", prefix, next, curr, suffix);
+
+    words.insert(temp);
 }
 
 fn read_input<R: Read>(mut reader: R) -> Vec<String> {
@@ -181,7 +136,7 @@ fn read_train_file() -> String {
 }
 
 fn process_input(input: String) -> Vec<String> {
-    input.split_whitespace()
+    input.split(|c:char| !c.is_alphanumeric())
         .filter(|word| !word.is_empty())
         .map(|word| word.to_lowercase())
         .collect()
@@ -197,4 +152,129 @@ fn count_words(words: Vec<String>) -> HashMap<String, usize> {
         }
     }
     counts
+}
+
+#[cfg(test)]
+mod spell_check_tests {
+    use super::{spell_check, check_insert, check_delete, check_replace, check_transpose};
+    use std::io::Cursor;
+    use std::collections::{HashMap, HashSet};
+
+    #[test]
+    fn generates_correct_insertions() {
+        let mut words:HashSet<String> = HashSet::new();
+        check_insert("alice", &mut words, 1);
+        assert!(words.contains("ablice"));
+        assert!(words.contains("azlice"));
+    }
+
+    #[test]
+    fn generates_correct_deletions() {
+        let mut words:HashSet<String> = HashSet::new();
+        check_delete("alice", &mut words, 1);
+        assert!(words.contains("aice"));
+    }
+
+    #[test]
+    fn generates_correct_deletions_on_edge() {
+        let mut words:HashSet<String> = HashSet::new();
+        check_delete("alice", &mut words, 0);
+        assert!(words.contains("lice"));
+        check_delete("alice", &mut words, 4);
+        assert!(words.contains("alic"));
+    }
+
+    #[test]
+    fn generates_correct_replacement() {
+        let mut words:HashSet<String> = HashSet::new();
+        check_replace("alice", &mut words, 1);
+        assert!(words.contains("ajice"));
+    }
+
+    #[test]
+    fn generates_correct_replacement_on_edge() {
+        let mut words:HashSet<String> = HashSet::new();
+        check_replace("alice", &mut words, 0);
+        assert!(words.contains("dlice"));
+        check_replace("alice", &mut words, 4);
+        assert!(words.contains("alicx"));
+    }
+
+    #[test]
+    fn generates_correct_transpositions() {
+        let mut words:HashSet<String> = HashSet::new();
+        check_transpose("alice", &mut words, 1);
+        assert!(words.contains("ailce"));
+    }
+
+    #[test]
+    fn generates_correct_transpositions_on_edge() {
+        let mut words:HashSet<String> = HashSet::new();
+        check_transpose("alice", &mut words, 0);
+        assert!(words.contains("laice"));
+        check_transpose("alice", &mut words, 3);
+        assert!(words.contains("aliec"));
+        check_transpose("alice", &mut words, 4);
+        assert!(!words.contains("alice"));
+    }
+
+    //#[test]
+    //fn picks_most_probable_word() {
+        //let mut dict:HashMap<String, usize> = HashMap::new();
+        //let mut words:HashSet<String> = HashSet::new();
+
+
+        //check_transpose("alice", &mut words, 1);
+        //for word in &words {
+            //println!("{}", word);
+        //}
+        //assert!(words.contains("ailce"));
+    //}
+
+    #[test]
+    fn corrects_typo_with_edit_distance_1() {
+        let mut dict:HashMap<String, usize> = HashMap::new();
+        dict.insert("banana".to_string(), 2);
+        dict.insert("alice".to_string(), 4);
+        dict.insert("blimps".to_string(), 3);
+        dict.insert("word".to_string(), 6);
+
+        assert_write("blice, alice", "blice", &dict);
+    }
+
+    #[test]
+    fn corrects_typo_with_edit_distance_2() {
+        let mut dict:HashMap<String, usize> = HashMap::new();
+        dict.insert("banana".to_string(), 2);
+        dict.insert("alice".to_string(), 4);
+        dict.insert("blimps".to_string(), 3);
+        dict.insert("word".to_string(), 6);
+        assert_write( "blce, alice", "blce", &dict);
+    }
+
+    #[test]
+    fn no_correction_found() {
+        let mut dict:HashMap<String, usize> = HashMap::new();
+        dict.insert("banana".to_string(), 2);
+        dict.insert("alice".to_string(), 4);
+        dict.insert("blimps".to_string(), 3);
+        dict.insert("word".to_string(), 6);
+        assert_write( "zzkjcbjbfkasbjasbajkda, -", "zzkjcbjbfkasbjasbajkda", &dict);
+    }
+
+    #[test]
+    fn input_is_correct() {
+        let mut dict:HashMap<String, usize> = HashMap::new();
+        dict.insert("banana".to_string(), 2);
+        dict.insert("alice".to_string(), 4);
+        dict.insert("blimps".to_string(), 3);
+        dict.insert("word".to_string(), 6);
+        assert_write( "alice", "alice", &dict);
+    }
+
+    fn assert_write(expected: &str, input: &str, dictionary:&HashMap<String, usize>) {
+        let mut writer = Cursor::new(vec![]);
+        spell_check(&mut writer, input, dictionary, 2);
+        assert_eq!(expected.as_bytes(), &*writer.into_inner())
+    }
 }
